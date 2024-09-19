55fa4a8623a089222f35d88044418f4f79fa52bd
Kyle Ingham <kyleingy@proton.me>
2024-09-19 17:51:02 -0600

    Successfully created post request to add data to the grocery_list_items table.

--- controllers/list-controller.js
+++ controllers/list-controller.js
@@ -7,17 +7,21 @@ const addItemToList = async (req, res) => {
   try {
     if (
       !req.body.grocery_list_id ||
-      !req.body.user_item_id ||
+      !req.body.item_name ||
       req.body.active_state === null ||
-      !req.body.province
+      !req.body.province ||
+      !req.body.category
     ) {
       return res
         .status(400)
         .json("Please provide all required item information");
     }
     const updatedList = await knex("grocery_list_items").insert(req.body);
-    const newItem = updatedList[0];
-    console.log(newItem);
+    const newItemId = updatedList[0];
+    const newItem = await knex("grocery_list_items")
+      .where({ id: newItemId })
+      .select("*");
+    res.status(201).json(newItem);
   } catch (error) {
     res.status(500).json(`${error}`);
   }
